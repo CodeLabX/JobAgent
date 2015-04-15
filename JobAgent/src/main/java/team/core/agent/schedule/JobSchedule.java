353dbd203387166f600e9ee99cdfb7fd54995414
cnn237111 <cnn237111@126.com>
2015-04-15 15:28:39 +0800

    update code

--- JobAgent/src/main/java/team/core/agent/schedule/JobSchedule.java
+++ JobAgent/src/main/java/team/core/agent/schedule/JobSchedule.java
@@ -1,11 +1,4 @@
 package team.core.agent.schedule;
-<<<<<<< HEAD
-import java.io.IOException;
-import java.util.Properties;
-=======
-
-
->>>>>>> origin/Dev
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.TimeUnit;
 
@@ -14,37 +7,8 @@ import java.util.concurrent.TimeUnit;
  */
 public class JobSchedule {
     public static void main(String[] args) {
-<<<<<<< HEAD
-
-/*        System.out.println(System.getProperty("user.dir"));
-
-=======
->>>>>>> origin/Dev
-        String[] paths = System.getProperty("user.dir").split("\\\\");
-        StringBuffer sb = new StringBuffer();
-        for (int i = 0; i < paths.length - 1; i++) {
-            sb.append(paths[i] + "\\");
-        }
-        String urlStr = sb.toString() +"jarlib\\"+ "DemoTask.jar";
-        System.out.println("jar包："+urlStr);
-<<<<<<< HEAD
-
-       */
-
-/*        Properties configFile = new Properties();
-        try {
-            configFile.load(JobSchedule.class.getClassLoader().getResourceAsStream("config.properties"));
-            String name = configFile.getProperty("jar_path");
-            System.out.println(name);
-        } catch (Exception e) {
-
-            e.printStackTrace();
-        }*/
-
-
         String urlStr = ConfigHelper.ReadConfig("jar_path")+ "\\DemoTask.jar";
-=======
->>>>>>> origin/Dev
+
         String className = "demo.task.mytask";
         String[] params = null;
 
